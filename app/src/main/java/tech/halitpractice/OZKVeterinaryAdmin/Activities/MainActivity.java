916f7ac038b1e2ffc40a00a1f4d5d6527f0b1fa2
Halit <egitimtraining@gmail.com>
2021-12-31 00:47:07 +0300

    Home Fragment

---
+++ app/src/main/java/tech/halitpractice/OZKVeterinaryAdmin/Activities/MainActivity.java
@@ -0,0 +1,21 @@
+package tech.halitpractice.OZKVeterinaryAdmin.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tech.halitpractice.OZKVeterinaryAdmin.Fragments.HomeFragment;
+import tech.halitpractice.OZKVeterinaryAdmin.R;
+import tech.halitpractice.OZKVeterinaryAdmin.Utils.ChangeFragments;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ChangeFragments changeFragments = new ChangeFragments(MainActivity.this);
+        changeFragments.change(new HomeFragment());
+    }
+}
---
+++ app/src/main/java/tech/halitpractice/OZKVeterinaryAdmin/Utils/ChangeFragments.java
@@ -0,0 +1,38 @@
+package tech.halitpractice.OZKVeterinaryAdmin.Utils;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import tech.halitpractice.OZKVeterinaryAdmin.R;
+
+public class ChangeFragments {
+
+    private Context context;
+
+    public ChangeFragments(Context context){
+        this.context = context;
+    }
+
+    public void change(Fragment fragment){
+        ((FragmentActivity)context).getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainFrameLayout,fragment,"fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+//                .addToBackStack(null).commit();
+    }
+
+    public void changeWithParameters(Fragment fragment,String petId){
+        Bundle bundle = new Bundle();
+        bundle.putString("petid",petId);
+        fragment.setArguments(bundle);
+        ((FragmentActivity)context).getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainFrameLayout,fragment,"fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
+    }
+
+}
